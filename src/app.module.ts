047699023dc7fc8d046cd7f0e643e212b9487708
dobecome <sylvius4@gmail.com>
2022-10-27 00:08:51 +0900

    init board

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, BoardModule],
   controllers: [AppController],
   providers: [AppService],
 })
